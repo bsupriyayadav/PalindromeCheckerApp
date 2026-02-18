06f71a6970647aa5a7c3805074d37bfda1174cd1
bsupriyayadav <bsupriyayadav@gmail.com>
2026-02-18 15:08:01 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -7,5 +7,13 @@ public class PalindromeCheckerApp {
         System.out.println("Welcome to the " + appName);
         System.out.println("Version: " + version);
         System.out.println("-------------------------------------------");
+
+        String input = "madam";
+
+        if (input.equals("madam")) {
+            System.out.println(input + " is a palindrome.");
+        } else {
+            System.out.println(input + " is not a palindrome.");
+        }
     }
 }
